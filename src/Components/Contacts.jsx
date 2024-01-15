4cbccd2cc5709546844ab4fe042c217d8994d342
Eugen Oswald <oswaldeugen95@gmail.com>
2024-01-15 20:16:09 +0100

    feat(contacts) add useEffect fetchUsers

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../scss/board.scss';
+
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from './Firebase';
 
 const Contacts = ({ userData }) => {
+
+    const [userList, setUserList] = useState([]);
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			const usersCollectionRef = collection(db, 'users');
+			const data = await getDocs(usersCollectionRef);
+			setTodos(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		};
+
+		fetchUsers();
+    }, []);
+    
+
 	return (
 		<div className='d-flex align-items-center justify-content-center w-100'>
 			<p>
